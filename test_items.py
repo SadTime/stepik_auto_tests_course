6d7245efc23e4d50f71973638118223c5edeba57
Sanya <alex0303275@gmail.com>
2022-09-06 17:10:24 +0300

    Fix mistakes

--- test_items.py
+++ test_items.py
@@ -6,5 +6,6 @@ link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_cart_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     time.sleep(10)
+    assert button, "Button is not found"
